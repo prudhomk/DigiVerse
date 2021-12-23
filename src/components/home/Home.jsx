00a87f96537520f7ad8403a393a833d6edbe6330
Kalan Prudhomme <cleaver9@gmail.com>
2021-12-23 09:39:48 -0800

    File setup

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the DigiVerse!</h1>
+      <p>Utilize our API in your projects to include the vast assortment of Digimon</p>
+      <p>Alternatively, checkout our DigiViewer to look through the catalog, or add additional Digimon!</p>
+    </div>
+  );
+}
