dc65f7f45199a336d397e00b3c29f73b8a43e7a8
Kalan Prudhomme <cleaver9@gmail.com>
2021-12-30 13:39:12 -0800

    added details, reworked some links for separate pages

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable max-len */
 import React from 'react';
-import DigimonList from '../digimon/DigimonList';
+import { useHistory } from 'react-router';
+
 
 export default function Home() {
+
+  const history = useHistory();
+
+  const handleDigimon = () => {
+    history.push('/digimon');
+  };
+
   return (
     <div>
       <h1>Welcome to the DigiVerse!</h1>
-      <DigimonList/>
       <p>Utilize our API in your projects to include the vast assortment of Digimon</p>
       <p>Alternatively, checkout our DigiViewer to look through the catalog, or add additional Digimon!</p>
+      <button onClick={handleDigimon}>DigiViewer</button>
     </div>
   );
 }
